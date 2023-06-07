f3a2cf9c604cc0db1bb83f48b0fcde3410c8ce37
Rhaqim <anusiemj@gmail.com>
2023-06-07 17:31:33 +0100

    working on the navigation

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,7 @@
+'use client';
+
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
-import React from 'react';
 
 type NavProps = {
   href: string;
@@ -7,6 +9,12 @@ type NavProps = {
 };
 
 const Nav = ({ navLinks }: { navLinks: NavProps[] }) => {
+  const [dropdownOpen, setDropdownOpen] = useState<boolean>(false);
+
+  const handleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
   return (
     <motion.nav
       initial={{ y: -100, opacity: 0 }}
@@ -14,15 +22,38 @@ const Nav = ({ navLinks }: { navLinks: NavProps[] }) => {
       transition={{ duration: 0.5 }}
       className="flex justify-between items-center p-8"
     >
-      {navLinks.map((link) => (
-        <a
-          key={link.href}
-          href={link.href}
-          className="border border-black p-2 bg-transparent rounded-lg transition duration-300 ease-in-out"
-        >
-          {link.label}
-        </a>
-      ))}
+      <div>
+        <p>Rhaqim</p>
+      </div>
+
+      {/* Hamburger */}
+      {!dropdownOpen && (
+        <div className="flex flex-col items-center justify-center">
+          <button
+            onClick={handleDropdown}
+            className="flex flex-col items-center justify-center space-y-2"
+          >
+            <div className="w-8 h-1 bg-black"></div>
+            <div className="w-8 h-1 bg-black"></div>
+            <div className="w-8 h-1 bg-black"></div>
+          </button>
+        </div>
+      )}
+
+      {/* Drop Down */}
+      {dropdownOpen && (
+        <div className="flex flex-col items-center m-4 border border-black p-2 rounded-lg ">
+          {navLinks.map((link) => (
+            <a
+              key={link.href}
+              href={link.href}
+              className="bg-transparent border-b-4 w-full border-black transition duration-300 ease-in-out"
+            >
+              {link.label}
+            </a>
+          ))}
+        </div>
+      )}
     </motion.nav>
   );
 };
