10675c85287d1f43a85e815c541fc3a3eb6899b2
Rhaqim <anusiemj@gmail.com>
2023-06-10 21:55:42 +0100

    loading animation

--- src/app/home/loading.tsx
+++ src/app/home/loading.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const loading = () => {
-  return <div>loading...</div>;
+const LoadingAnimation = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="flex space-x-2 animate-bounce">
+        <div className="w-4 h-4 bg-gray-800 rounded-full"></div>
+        <div className="w-4 h-4 bg-gray-800 rounded-full"></div>
+        <div className="w-4 h-4 bg-gray-800 rounded-full"></div>
+      </div>
+    </div>
+  );
 };
 
-export default loading;
+export default LoadingAnimation;
