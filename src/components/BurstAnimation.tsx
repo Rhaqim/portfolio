685e4a7c994df23fca0b7a873641d521f69153fe
Rhaqim <anusiemj@gmail.com>
2023-06-11 16:04:55 +0100

    working on the positioning for the artefacts

--- src/components/BurstAnimation.tsx
+++ src/components/BurstAnimation.tsx
@@ -2,6 +2,7 @@
 
 import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
+import Anim from './Anim';
 
 const images = [
   'https://images.unsplash.com/photo-1574169208507-84376144848b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1179&q=80',
@@ -11,15 +12,29 @@ const images = [
 
 const randomPosition = () => {
   const maxDistance = 200; // Adjust the maximum distance from the center
-  const x = - Math.random() * maxDistance - maxDistance / 10;
+  const x = -Math.random() * maxDistance - maxDistance / 10;
   const y = Math.random() * maxDistance - maxDistance / 10;
   return { x, y };
 };
 
 const BurstAnimation = () => {
-  const [height, setHeight] = useState(0);
-  const [width, setWidth] = useState(0);
-
+  const artefacts = [
+    {
+      name: 'Artefact 1',
+      description: 'This is the first artefact',
+      arteface: <Anim />,
+    },
+    {
+      name: 'Artefact 2',
+      description: 'This is the second artefact',
+      arteface: <Anim />,
+    },
+    {
+      name: 'Artefact 3',
+      description: 'This is the third artefact',
+      arteface: <Anim />,
+    },
+  ];
   const containerVariants = {
     hidden: { opacity: 0 },
     visible: {
@@ -36,13 +51,37 @@ const BurstAnimation = () => {
     visible: { scale: 1, opacity: 1 },
   };
 
+  const [screenWidth, setScreenWidth] = useState(0);
+  const [screenHeight, setScreenHeight] = useState(0);
+  const [middleX, setMiddleX] = useState(0);
+  const [middleY, setMiddleY] = useState(0);
+
   useEffect(() => {
-    setHeight(window.innerHeight);
-    setWidth(window.innerWidth);
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+      setScreenHeight(window.innerHeight);
+    };
+
+    // Initial size on mount
+    handleResize();
+
+    // Event listener for window resize
+    window.addEventListener('resize', handleResize);
+
+    // Clean up the event listener on unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
   }, []);
 
-  const middleX = width / 2;
-  const middleY = height / 2;
+  useEffect(() => {
+    const calculateMiddlePoint = () => {
+      setMiddleX(screenWidth / 2);
+      setMiddleY(screenHeight / 2);
+    };
+
+    calculateMiddlePoint();
+  }, [screenWidth, screenHeight]);
 
   return (
     <div
@@ -61,20 +100,21 @@ const BurstAnimation = () => {
         initial="hidden"
         animate="visible"
       >
-        {images.map((imageUrl, index) => (
+        {artefacts.map((imageUrl, index) => (
           <motion.div
             key={index}
             variants={imageVariants}
             initial={{
               opacity: 1,
               scale: 0.2,
-              x: middleX,
-              y: middleY,
+              x: 0, // Start the artifacts from the center of the screen
+              y: 0, // Start the artifacts from the center of the screen
             }}
             animate={{
               opacity: 1,
               scale: 1,
-              ...randomPosition(),
+              x: randomPosition().x, // Spread out the artifacts horizontally
+              y: randomPosition().y, // Spread out the artifacts vertically
             }}
             transition={{
               type: 'slide',
@@ -84,14 +124,18 @@ const BurstAnimation = () => {
               delay: index * 0.2,
             }}
             style={{
-              backgroundImage: `url('${imageUrl}')`,
+              // backgroundImage: `url('${imageUrl}')`,
               width: '200px',
               height: '200px',
               backgroundSize: 'cover',
               backgroundPosition: 'center',
               position: 'absolute',
+              left: `calc(${middleX}px - 100px)`, // Adjust the left position to center the artifacts horizontally
+              top: `calc(${middleY}px - 100px)`, // Adjust the top position to center the artifacts vertically
             }}
-          />
+          >
+            {artefacts[index].arteface}
+          </motion.div>
         ))}
       </motion.div>
     </div>
