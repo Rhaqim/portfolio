89a6916121195f7ea493b8386b5b82e6d347fc46
Rhaqim <anusiemj@gmail.com>
2023-12-21 17:22:00 +0100

    projects and open source

---
+++ src/constants/opensource.ts
@@ -0,0 +1,76 @@
+export const opensourceProjects = [
+  {
+    title: 'Jobs We Want',
+    description:
+      'A helpful extension for tracking jobs you want to or have applied to.',
+    link: null,
+    github: 'https://github.com/Rhaqim/jobs_we_want',
+    tags: ['Jobs', 'Metrics', 'Tracker', 'Extension'],
+    toolkits: [
+      {
+        name: 'JavaScript',
+        logo: 'javascript',
+      },
+      {
+        name: 'HTML',
+        logo: 'html',
+      },
+      {
+        name: 'CSS',
+        logo: 'css',
+      },
+      {
+        name: 'Git',
+        logo: 'git',
+      },
+      {
+        name: 'Github',
+        logo: 'github',
+      },
+      {
+        name: 'VC Code',
+        logo: 'vscode',
+      },
+    ],
+    images: [],
+  },
+  {
+    title: 'Ethereum Node Tester CLI',
+    description:
+      'A CLI tool to test the functions on Ethereum nodes. It can be used to test the connection to Ethereum nodes and check the status of the nodes.',
+    link: null,
+    github: 'https://github.com/Rhaqim/node_tester_cli',
+    tags: ['Web3', 'CLI', 'Ethereum'],
+    toolkits: [
+      {
+        name: 'Rust',
+        logo: 'rust',
+      },
+      {
+        name: 'Linux',
+        logo: 'linux',
+      },
+      {
+        name: 'Bash',
+        logo: 'bash',
+      },
+      {
+        name: 'Solidity',
+        logo: 'solidity',
+      },
+      {
+        name: 'Git',
+        logo: 'git',
+      },
+      {
+        name: 'Github',
+        logo: 'github',
+      },
+      {
+        name: 'VC Code',
+        logo: 'vscode',
+      },
+    ],
+    images: [],
+  },
+];
--- src/constants/workCategories.tsx
+++ src/constants/workCategories.tsx
@@ -1,5 +1,8 @@
 import WorkItem from '@/components/Work/WorkItem';
 import { workExperiences } from './WorkExperience';
+import { personalProjects } from './personalProjects';
+import Projects from '@/components/Work/Projects';
+import { opensourceProjects } from './opensource';
 
 const workCategories = [
   {
@@ -23,7 +26,13 @@ const workCategories = [
     image:
       'https://images.unsplash.com/photo-1681163101469-5175a8925526?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1772&q=80',
     duration: 3,
-    content: <div>Personal</div>,
+    content: (
+      <div>
+        {personalProjects.map((project, index) => (
+          <Projects key={index} {...project} />
+        ))}
+      </div>
+    ),
   },
   {
     name: 'Open Source //#endregion',
@@ -31,7 +40,13 @@ const workCategories = [
     image:
       'https://images.unsplash.com/photo-1672717892736-d3fcf1cd23c1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1771&q=80',
     duration: 4,
-    content: <div>Open Source</div>,
+    content: (
+      <div>
+        {opensourceProjects.map((project, index) => (
+          <Projects key={index} {...project} />
+        ))}
+      </div>
+    ),
   },
 ];
 
