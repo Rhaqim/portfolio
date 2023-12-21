01e386972a500e0f71cc627fef7e81d6a965d5f7
Rhaqim <anusiemj@gmail.com>
2023-12-21 04:22:23 +0100

    view and download resume

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+'use client';
+
+import React from 'react';
 
 const Resume = () => {
-  return (
-    <div>Resume</div>
-  )
-}
+  const handleDownloadResume = () => {
+    // Logic to download the resume file
+    const file = '/resume/profile.pdf';
+    window.open(file);
+    const link = document.createElement('a');
+    link.href = file;
+    link.download = 'AnusiemJohnFranklinResume.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
 
-export default Resume
+  const handleViewResume = () => {
+    // Logic to view the resume file
+    const file = '/resume/profile.pdf';
+    window.open(file);
+  };
+
+  return (
+    <div>
+      <h1>Resume</h1>
+      <button onClick={handleDownloadResume}>Download Resume</button>
+        <button onClick={handleViewResume}>View Resume</button>
+    </div>
+  );
+};
+
+export default Resume;
