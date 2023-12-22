2b6676b926a6d7c693b5557b7859e2d01e22e45b
Rhaqim <anusiemj@gmail.com>
2023-12-22 06:21:35 +0100

    using iframe to display the resume and moving the comments to the dynamic route

--- src/app/work/blog/page.tsx
+++ src/app/work/blog/page.tsx
@@ -1,73 +1,9 @@
-'use client';
+import React from 'react'
 
-// InteractiveBlog.tsx
-import React, { useState } from 'react';
-import { motion } from 'framer-motion';
-import GameThemedAnimations from '@/components/Anim/GameThemed';
-
-const InteractiveBlog: React.FC = () => {
-  const [comments, setComments] = useState<string[]>([]);
-  const [newComment, setNewComment] = useState('');
-
-  const handleCommentSubmit = () => {
-    if (newComment.trim() !== '') {
-      setComments([...comments, newComment]);
-      setNewComment('');
-    }
-  };
-
+const Blog = () => {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold mb-4">Interactive Blog</h1>
+    <div>Blog</div>
+  )
+}
 
-      <motion.div
-        initial={{ opacity: 0, y: -100 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 1 }}
-        className="bg-gray-200 p-8 rounded-md shadow-md text-black"
-      >
-        <h2 className="text-2xl font-bold mb-4">Latest Post</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac libero
-          vel erat volutpat malesuada.
-        </p>
-
-        {/* Gamified Element */}
-        <motion.div
-          whileHover={{ scale: 1.1 }}
-          className="mt-4 p-4 bg-blue-500 text-white cursor-pointer"
-        >
-          Clap for this post! 👏
-        </motion.div>
-
-        {/* Comment Section */}
-        <div className="mt-8">
-          <h3 className="text-xl font-bold mb-2">Comments</h3>
-          <ul>
-            {comments.map((comment, index) => (
-              <li key={index} className="mb-2">
-                {comment}
-              </li>
-            ))}
-          </ul>
-          <div className="mt-4">
-            <textarea
-              value={newComment}
-              onChange={(e) => setNewComment(e.target.value)}
-              className="w-full p-2 border border-gray-300 rounded"
-              placeholder="Add a comment..."
-            />
-            <button
-              onClick={handleCommentSubmit}
-              className="mt-2 bg-blue-500 text-white p-2 rounded"
-            >
-              Submit Comment
-            </button>
-          </div>
-        </div>
-      </motion.div>
-    </div>
-  );
-};
-
-export default InteractiveBlog;
+export default Blog
--- src/app/work/resume/page.tsx
+++ src/app/work/resume/page.tsx
@@ -22,10 +22,31 @@ const Resume = () => {
   };
 
   return (
-    <div>
-      <h1>Resume</h1>
-      <button onClick={handleDownloadResume}>Download Resume</button>
-      <button onClick={handleViewResume}>View Resume</button>
+    <div className="flex items-center justify-center flex-col">
+      <h1 className="text-4xl font-bold mb-4">Resume</h1>
+      {/* Display resume */}
+      <iframe
+        src={file}
+        width="100%"
+        height="700px"
+        title="Resume"
+        className="mt-4"
+      />
+
+      {/* <div className="flex w-full justify-evenly">
+        <button
+          className="mt-4 p-4 bg-blue-500 text-white cursor-pointer"
+          onClick={handleDownloadResume}
+        >
+          Download Resume
+        </button>
+        <button
+          className="mt-4 p-4 bg-blue-500 text-white cursor-pointer"
+          onClick={handleViewResume}
+        >
+          View Resume
+        </button>
+      </div> */}
     </div>
   );
 };
