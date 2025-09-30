b53e6ede94e6c1424124be371c5b2dd1852664b6
Rhaqim <anusiemj@gmail.com>
2025-09-30 08:22:30 +0100

    make site static

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,10 @@
 import { defineConfig } from "astro/config";
 
 import svelte from "@astrojs/svelte";
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://rhaqim.com",
 	integrations: [svelte()],
-	output: "server",
-	adapter: node({ mode: "standalone" }),
+	output: "static",
 });
